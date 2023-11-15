71c1597af4a1878084a2c6ab760cdd5fbccb73c5
JackSteffan <150395486+JackSteffan@users.noreply.github.com>
2023-11-15 16:55:54 -0600

    Need to edit this later to make it only the currently logged in member that shows on the gridview

--- NewAssignment4/MyMembers/MemberPage.aspx.cs
+++ NewAssignment4/MyMembers/MemberPage.aspx.cs
@@ -21,7 +21,7 @@ namespace NewAssignment4.MyMembers
         {
             dbcon = new KarateDataContext(conn);
 
-            var result = from x in dbcon.Sections select x;
+            var result = from x in dbcon.Sections select x;// need to edit this to make it the logged in memeber only.
 
             GridView1.DataSource = result;
             GridView1.DataBind();
