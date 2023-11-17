3791887e32a98d9062db6aa29a48e66809f30e08
Clovotc <maison.kasprick@gmail.com>
2023-11-17 14:28:57 -0600

    Added usertype validation to all pages

--- NewAssignment4/MyMembers/MemberPage.aspx.cs
+++ NewAssignment4/MyMembers/MemberPage.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -15,6 +16,20 @@ namespace NewAssignment4.MyMembers
         protected void Page_Load(object sender, EventArgs e)
         {
             RefreshData();
+
+            if (Session.Count != 0)
+            {
+                if (HttpContext.Current.Session["userType"].ToString().Trim().ToLower() == "instructor" ||
+                    HttpContext.Current.Session["userType"].ToString().Trim().ToLower() == "administrator")
+                {
+                    Session.Clear();
+                    Session.RemoveAll();
+                    Session.Abandon();
+                    Session.Abandon();
+                    FormsAuthentication.SignOut();
+                    Response.Redirect("Logon.aspx", true);
+                }
+            }
         }
 
         private void RefreshData()
