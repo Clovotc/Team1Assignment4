2d5b7a748d77d25ca243a0565f8b5b5a91d3fe64
JackSteffan <150395486+JackSteffan@users.noreply.github.com>
2023-11-17 15:54:34 -0600

    Finished the MembersPage. Also made a minor change in making the text color white in the sitemaster.

--- NewAssignment4/MyMembers/MemberPage.aspx.cs
+++ NewAssignment4/MyMembers/MemberPage.aspx.cs
@@ -21,7 +21,7 @@ namespace NewAssignment4.MyMembers
         {
             dbcon = new KarateDataContext(conn);
 
-            var result = from x in dbcon.Sections select x;// need to edit this to make it the logged in memeber only.
+            var result = from x in dbcon.Sections where x.Member.ToString() == LoginName1.ToString() select x;// need to edit this to make it the logged in memeber only.
 
             GridView1.DataSource = result;
             GridView1.DataBind();
